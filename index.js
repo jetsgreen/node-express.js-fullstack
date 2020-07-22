519b53b9366d406ef9ac29935e8e28a2a86b9ed9
Felix Fernandez <fernandezfelix460.gmail.com>
2020-07-22 12:20:10 -0400

    installed pug as a template engine, set up several views, added database, installed mongoose as an ORM, also installed bootstrap

--- index.js
+++ index.js
@@ -1,14 +1,92 @@
 const express = require('express');
+const path = require('path');
+var bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/nodeExpress');
+let db = mongoose.connection;
+
+// check connection
+db.once('open', () => {
+    console.log('Connected to MongoDb')
+})
+
+// check for db erros
+db.on('error', (err) => {
+    console.log(Error)
+});
+
+// Init App
 const app = express();
 
+// Bring in models
+let Article = require('./models/articles');
 
-app.get("/", (req, res)=>{
-    res.send('hello world')
+// Set up the template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Body parser middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get("/", (req, res) => {
+    Article.find({}, (err, articles) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('index', {
+                title: 'Articles',
+                articles: articles
+            });
+        };
+
+    });
+
+});
+
+// get single article
+app.get('/article/:id', (req, res)=>{
+    Article.findById(req.params.id, (error, article)=>{
+        res.render('article', {
+            article:article
+        })
+    })
 })
+
+app.get("/articles/add", (req, res) => {
+    res.render('add_article', {
+        title: 'Add Article'
+    })
+});
+
+// Add Submit Post Rout
+app.post('/articles/add', (req, res)=>{
+ let article = new Article();
+ article.title = req.body.title;
+ article.author = req.body.author;
+ article.body = req.body.body;
+
+ article.save((err)=>{
+     if(err){
+        console.log(err);
+        return;
+     }else{
+         res.redirect('/');
+     }
+ })
+});
 
 const PORT = 1000;
 
-
-app.listen(1000, ()=>{
+// Start Server
+app.listen(1000, () => {
     console.log(`Server listening on port: ${PORT}`)
 })
