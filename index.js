80ba480011353eb8817dc131c7112d0b89adebc7
Felix Fernandez <fernandezfelix460.gmail.com>
2020-07-23 10:23:54 -0400

    created delete button and the functionality that goes with it, added jquery for ajax delete call

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get('/article/:id', (req, res)=>{
             article:article
         })
     })
-})
+});
 
 app.get("/articles/add", (req, res) => {
     res.render('add_article', {
@@ -83,6 +83,45 @@ app.post('/articles/add', (req, res)=>{
      }
  })
 });
+
+// Edit single Article
+app.get('/article/edit/:id', (req, res)=>{
+    Article.findById(req.params.id, (error, article)=>{
+        res.render('edit_article', {
+            title: 'Edit Article',
+            article:article
+        })
+    })
+});
+// Update single article
+app.post('/articles/edit/:id', (req, res)=>{
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    let query = {_id:req.params.id}
+   
+    Article.update(query, article, (err)=>{
+        if(err){
+           console.log(err);
+           return;
+        }else{
+            res.redirect('/');
+        }
+    })
+   });
+
+ app.delete('/article/:id', (req, res)=>{
+    let query = {_id:req.params.id}
+
+    Article.remove(query, (err)=>{
+        if(err){
+            console.log(err)
+        }
+        res.send('Success');
+    })
+ })  
 
 const PORT = 1000;
 
